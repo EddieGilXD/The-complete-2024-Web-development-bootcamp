28da8195282a76895d32f6174078d011d42a0e3e
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-11 23:07:39 -0600

    Introducton to JS done

---
+++ Section 15 - Intermediate JavaScript/Function part 2 - Parameters and arguments/exercise.js
@@ -0,0 +1,28 @@
+function getMilk(dollars) {
+  let bottleMilk = 1.5;
+
+  let bottles = Math.floor(dollars / bottleMilk);
+  
+  
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log(`Buy ${bottles} bottles of milk`);
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(4);
